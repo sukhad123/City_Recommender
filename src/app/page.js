ddd78314903544f0bed7bdd7eebb4f9c7fbae100
Gordon Tan <gordonsortan@gmail.com>
2025-09-24 14:35:27 -0400

    Add re-usable footer element and add footer to review page and navbar

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import NavbarComponent from "./components/composite/Navbar";
 import { unauthenticatedMenuItems } from "../constants/Navbar/constants.js";
 import { Button } from "@heroui/react";
 import Link from "next/link";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -46,17 +47,7 @@ export default function Home() {
       </div>
 
    
-      <footer className="w-full border-t border-default-200 dark:border-default-100 py-4 mt-12">
-  <div className="max-w-7xl mx-auto px-6 flex flex-col sm:flex-row justify-between items-center text-sm text-gray-600 dark:text-gray-400">
-    <span>© {new Date().getFullYear()} City Recommender. All rights reserved.</span>
-    <div className="flex space-x-4 mt-2 sm:mt-0">
-   <span className="hover:underline cursor-pointer text-gray-500">About</span>
-<span className="hover:underline cursor-pointer text-gray-500">Contact</span>
-<span className="hover:underline  cursor-pointer text-gray-500">Privacy</span>
-
-    </div>
-  </div>
-</footer>
+  <Footer />  
 
     </>
   );
--- src/app/review/page.js
+++ src/app/review/page.js
@@ -1,6 +1,7 @@
 import ReviewPageClient from "./components/ReviewPageClient";
 import NavbarComponent from "../components/composite/Navbar";
 import { unauthenticatedMenuItems } from "../../constants/Navbar/constants";
+import Footer from "../components/Footer";
 
 export default async function ReviewPage({ searchParams }) {
 
@@ -9,9 +10,13 @@ export default async function ReviewPage({ searchParams }) {
 
   return (
     <>
+     <NavbarComponent menuItems={unauthenticatedMenuItems}>
+       
+          </NavbarComponent>
     <div className="flex flex-col h-screen max-w-2xl mx-auto p-4 space-y-6">
       <ReviewPageClient initialCity={city} />  
     </div>
+    <Footer/>
     </>
   );
 }
