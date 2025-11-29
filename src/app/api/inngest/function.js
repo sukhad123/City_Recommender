40e1bf7e3472325b709096b64c428ecdd3bfd0c5
Sukhad Adhikari <101186753+sukhad123@users.noreply.github.com>
2025-11-29 15:24:18 -0500

    updated db -> updatedAt attr. for city-Cache

--- src/app/api/inngest/function.js
+++ src/app/api/inngest/function.js
@@ -1,6 +1,8 @@
 import { inngest } from "./client";
 import validate_update_city_info from "../../../services/core/validateCityData/check_validate_city_info"
 
+
+//validate_update_city
 export const validate_update_cityData = inngest.createFunction(
   { id: "validate_updateCityData" },
   { event: "cityData/validate_updateCityData" },
@@ -9,4 +11,15 @@ export const validate_update_cityData = inngest.createFunction(
   async () => {
     return await validate_update_city_info();
   },
+);
+
+//Test cron job
+export const testFunction= inngest.createFunction(
+  { id: "activation-email" },
+  { event: "app/test" },
+  async ({ event, step }) => {
+    await step.run("send-welcome-email", async () => {
+     return;
+    });
+  }
 );
